196fe26248cc868975a1b7ff9cf3aa590060cd27
Carlos Eduardo <Carlosedu44@outlook.com>
2024-10-02 15:14:32 -0300

    Update project

--- src/main/java/com/DescomplicandoIngles/DescomplicandoIngles/service/LessonService.java
+++ src/main/java/com/DescomplicandoIngles/DescomplicandoIngles/service/LessonService.java
@@ -50,6 +50,7 @@ public class LessonService {
                 .map(UserLessonInteraction::getLesson) // Mapeia cada interação para a lição associada
                 .collect(Collectors.toList());
     }
+    
 }
 
 
